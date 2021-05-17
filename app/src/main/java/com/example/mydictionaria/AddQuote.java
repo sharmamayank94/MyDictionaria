bcc64cd44d559da4e9584933e389bd7ae7c4c772
sharmamayank94 <sharmamayank941@gmail.com>
2021-05-17 23:49:19 +0530

    Added setOnClickListener to add quote button

--- app/src/main/java/com/example/mydictionaria/AddQuote.java
+++ app/src/main/java/com/example/mydictionaria/AddQuote.java
@@ -1,5 +1,26 @@
 package com.example.mydictionaria;
 
-public class AddQuote {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddQuote extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_quote);
+
+        Button addQuoteButton = findViewById(R.id.add_quote_button);
+
+        addQuoteButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+
+        });
+    }
 }
